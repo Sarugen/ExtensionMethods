d76bccd705f266c2c42c7d2e6c212ea388c9b940
Sarugen <sepenzev97@gmail.com>
2023-01-27 17:45:36 +0200

    New

--- ExtensionMethods1/Program.cs
+++ ExtensionMethods1/Program.cs
@@ -1,10 +1,33 @@
-﻿
+﻿using System;
+
 //int i = 2.4.Round();  // i = 2
 //int j = 2.5.Round();  // j = 3
 
-double i = Math.Round (2.4);  // i = 2
-double j = Math.Round (2.5);  // j = 3
 
 
+public class ExtensionMethod
+{
+    public static void Main()
+    {
+        double value = 2.4;
+        for (int i = 0; i <= 1; i++)
+            value = RoundValueAndAdd(value);
+
+        Console.WriteLine();
+
+        value = 2.5;
+        RoundValueAndAdd(value);
+
+    }
+
+    private static double RoundValueAndAdd(double value)
+    {
+        
+        Console.WriteLine("{0} --> {1}", value, Math.Round(value));
+        return value + .1;
+
+    }
+
+}
 
 
